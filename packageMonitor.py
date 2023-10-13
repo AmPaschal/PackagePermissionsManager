2c8ae1bf4e144a846f80c622e5ae77e9858d2fb7
Kyle <robin489@purdue.edu>
2023-10-13 15:45:54 -0400

    Added a variety of fixes to handle and log errors

--- packageMonitor.py
+++ packageMonitor.py
@@ -2,6 +2,12 @@ import psycopg2
 import subprocess
 import os
 import shutil
+import signal
+
+def keyboard_interrupt_handler(sig, frame):
+    raise KeyboardInterrupt
+
+signal.signal(signal.SIGINT, keyboard_interrupt_handler)
 
 # Establish a connection to the database
 try:
@@ -30,29 +36,35 @@ try:
     success_count = 0
     failure_count = 0
     for row in rows:
-        # Cloning each GitHub repository
-        subprocess.run(["git", "clone", row[0]])
-
-        # Extracting the repository name
-        repo_name = row[0].split("/")[-1].split(".")[0]
-
-        # Set environment variable MAVEN_OPTS
-        os.environ["MAVEN_OPTS"] = "-javaagent:/home/robin489/vulnRecreation/PackagePermissionsManager/target/PackagePermissionsManager-1.0-SNAPSHOT-perm-agent.jar=m10," + repo_name
-        
-
-        # Running the test suite using mvn as root
-        process = subprocess.run(["sudo", "-E", "mvn", "test", "-Dmaven.test.failure.ignore=true"], cwd=repo_name)
-        
-        if process.returncode != 0:
-            error_msg = f"Error occurred while running 'mvn test' in {repo_name}:\n"
-            #error_msg += process.stdout + process.stderr + "\n\n"
-            outputFilePath = "/home/robin489/vulnRecreation/jsons" + repo_name + "*"
-            failFolder = "/home/robin489/vulnRecreation/jsons/failures"
-            subprocess.run(["mv", outputFilePath, failFolder ])
-            log_file.write(error_msg)
+        try:
+            # Cloning each GitHub repository
+            subprocess.run(["git", "clone", row[0]])
+    
+            # Extracting the repository name
+            repo_name = row[0].split("/")[-1].split(".")[0]
+    
+            # Set environment variable MAVEN_OPTS
+            os.environ["MAVEN_OPTS"] = "-javaagent:/home/robin489/vulnRecreation/PackagePermissionsManager/target/PackagePermissionsManager-1.0-SNAPSHOT-perm-agent.jar=m10," + repo_name
+            
+    
+            # Running the test suite using mvn as root
+            process = subprocess.run(["sudo", "-E", "mvn", "test", "-Dmaven.test.failure.ignore=true"], cwd=repo_name, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+            
+            if process.returncode != 0:
+                error_msg = f"Error occurred while running 'mvn test' in {repo_name}:\n"
+                error_msg += process.stdout + process.stderr + "\n\n"
+                outputFilePath = "/home/robin489/vulnRecreation/jsons/0" + repo_name + "*"
+                failFolder = "/home/robin489/vulnRecreation/jsons/failures"
+                subprocess.run(["mv", outputFilePath, failFolder ])
+                log_file.write(error_msg)
+                failure_count += 1
+            else:
+                success_count+= 1
+        except KeyboardInterrupt:
             failure_count += 1
-        else:
-            success_count+= 1
+            log_file.write(f"Execution interrupted for {repo_name}\n")
+            continue
+        
         shutil.rmtree(repo_name, ignore_errors=True)
             
     log_file.write(f"Number of successes: {success_count}\n")
