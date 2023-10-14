ead8e5403514b4ff7945405b5e52ab9612f1dfeb
Kyle <robin489@purdue.edu>
2023-10-13 21:23:42 -0400

    Added additional logging ability

--- packageMonitor.py
+++ packageMonitor.py
@@ -2,7 +2,6 @@ import psycopg2
 import subprocess
 import os
 import shutil
-import signal
 import requests
 import json
 
@@ -10,10 +9,7 @@ github_api_url = "https://api.github.com/search/repositories"
 output_directory = "/home/robin489/vulnRecreation/dependentPackages"
 if not os.path.exists(output_directory):
     os.makedirs(output_directory)
-def keyboard_interrupt_handler(sig, frame):
-    raise KeyboardInterrupt
 
-signal.signal(signal.SIGINT, keyboard_interrupt_handler)
 
 # Establish a connection to the database
 try:
@@ -44,32 +40,35 @@ LIMIT 100;"""
     log_file = open("mvn_test_errors.log", "w")
     success_count = 0
     failure_count = 0
+    rows = list(set(rows))
     for row in rows:
-        try:
-            # Cloning each GitHub repository
-            subprocess.run(["git", "clone", row[0]])
+        
+        # Cloning each GitHub repository
+        subprocess.run(["git", "clone", row[0]])
     
-            # Extracting the repository name
-            repo_name = row[0].split("/")[-1].split(".")[0]
+        # Extracting the repository name
+        repo_name = row[0].split("/")[-1].split(".")[0]
     
-            # Set environment variable MAVEN_OPTS
-            os.environ["MAVEN_OPTS"] = "-javaagent:/home/robin489/vulnRecreation/PackagePermissionsManager/target/PackagePermissionsManager-1.0-SNAPSHOT-perm-agent.jar=m10," + repo_name
+        # Set environment variable MAVEN_OPTS
+        os.environ["MAVEN_OPTS"] = "-javaagent:/home/robin489/vulnRecreation/PackagePermissionsManager/target/PackagePermissionsManager-1.0-SNAPSHOT-perm-agent.jar=m10," + repo_name
             
     
-            # Running the test suite using mvn as root
-            process = subprocess.run(["sudo", "-E", "mvn", "test", "-Dmaven.test.failure.ignore=true"], cwd=repo_name, stderr=subprocess.PIPE, text=True)
+        # Running the test suite using mvn as root
+        process = subprocess.run(["sudo", "-E", "mvn", "test", "-Dmaven.test.failure.ignore=true"], cwd=repo_name, stderr=subprocess.PIPE, text=True)
             
-            if process.returncode != 0:
-                error_msg = f"Error occurred while running 'mvn test' in {repo_name}:\n"
-                error_msg += process.stderr + "\n\n"
-                outputFilePath = "/home/robin489/vulnRecreation/jsons/" + repo_name + "*"
-                failFolder = "/home/robin489/vulnRecreation/jsons/failures"
-                subprocess.run(["mv", outputFilePath, failFolder ])
-                print(error_msg)
-                log_file.write(error_msg)
-                failure_count += 1
-            else:
-                success_count+= 1
+        if process.returncode != 0:
+            error_msg = f"Error occurred while running 'mvn test' in {repo_name}:\n"
+            error_msg += process.stderr + "\n\n"
+            outputFilePath = "/home/robin489/vulnRecreation/jsons/" + repo_name + "*"
+            failFolder = "/home/robin489/vulnRecreation/jsons/failures"
+            subprocess.run(["mv", outputFilePath, failFolder ])
+            print(error_msg)
+            log_file.write(error_msg)
+            failure_count += 1
+        else:
+            success_count+= 1
+            success_msg = f"Succesfully wrote {repo_name}\n"
+            log_file.write(success_msg)
             package_name = row[0].split("/")[-1].split(".")[0]
             params = {
             "q": f"\"{package_name}\" in:dependency",
@@ -88,12 +87,9 @@ LIMIT 100;"""
             else:
                 print(f"Failed to retrieve data from GitHub API for {package_name}")
                 log_file.write(f"Failed to retrieve data from GitHub API for {package_name}")
-        except KeyboardInterrupt:
-            failure_count += 1
-            log_file.write(f"Execution interrupted for {repo_name}\n")
-            continue
         
-        shutil.rmtree(repo_name, ignore_errors=True)
+        
+    shutil.rmtree(repo_name, ignore_errors=True)
             
     log_file.write(f"Number of successes: {success_count}\n")
     log_file.write(f"Number of failures: {failure_count}\n")
