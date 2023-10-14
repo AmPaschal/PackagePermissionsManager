4d7d0fe48554739cc5e79f120ddc6147cf753941
Kyle <robin489@purdue.edu>
2023-10-14 13:36:36 -0400

    Adding changes to Permission Callback

--- src/main/java/com/ampaschal/google/PermissionsCallback.java
+++ src/main/java/com/ampaschal/google/PermissionsCallback.java
@@ -4,11 +4,11 @@ import com.ampaschal.google.enums.ResourceOp;
 import com.ampaschal.google.enums.ResourceType;
 
 import java.util.Set;
-import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 
 public interface PermissionsCallback {
 
-    void onPermissionRequested(LinkedHashMap<String, PermissionsObject> subject, int subjectPathSize, ResourceType resourceType, ResourceOp resourceOp, String resourceItem);
+    void onPermissionRequested(LinkedHashSet<String> subject, int subjectPathSize, ResourceType resourceType, ResourceOp resourceOp, String resourceItem);
 
     void onPermissionFailure(Set<String> subjectPaths, ResourceType resourceType, ResourceOp resourceOp, String resourceItem);
 
