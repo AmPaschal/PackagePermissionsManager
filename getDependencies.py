c60479d1aab489c5ed42b85b3bab50fb0eed78eb
Kyle <robin489@purdue.edu>
2023-10-16 21:16:59 -0400

    Added automatic directory management

--- getDependencies.py
+++ getDependencies.py
@@ -1,7 +1,7 @@
 import os
 import git
 import xml.etree.ElementTree as ET
-
+import shutil
 # List of GitHub URLs
 
 parent_directory = "./applicationDependencies"
@@ -16,8 +16,10 @@ def count_dependencies(pom_path, repo_name):
     tree = ET.parse(pom_path)
     root = tree.getroot()
     dependencies = root.findall(".//{http://maven.apache.org/POM/4.0.0}dependency")
-    dependListFile = f"{parent_directory}/{repo_name}/{repo_name}dependList.txt"
-    
+    dependDir = f"{parent_directory}/{repo_name}"
+    dependListFile = f"{dependDir}/{repo_name}dependList.txt"
+    if not os.path.exists(dependDir):
+        os.makedirs(dependDir)
     with open(dependListFile, "w") as f:
         f.write('\n'.join(dependencies))
             
@@ -26,6 +28,8 @@ def count_dependencies(pom_path, repo_name):
 
 input_file = "valid_repository_urls"
 github_urls = []
+if not os.path.exists(parent_directory):
+    os.makedires(parent_directory)
 if os.path.exists(input_file):
     with open(input_file, "r") as f:
         github_urls = [line.strip() for line in f.readlines()]
@@ -50,6 +54,6 @@ for url in github_urls:
 with open(output_file, "w") as f:
     for item in dependency_list:
         f.write("%s\n" % item)
-
+shutil.rmtree(clone_directory, ignore_errors=True)
 print(f"List of dependencies has been saved to {output_file}.")
 
