72dfa3ef207ba0693d083ac95a753d2bf7e8267d
Kyle <robin489@purdue.edu>
2023-10-18 05:21:51 -0400

    Made map

---
+++ makeMap.py
@@ -0,0 +1,37 @@
+import psycopg2
+repo_name = "jhipster-registry"
+try:
+    connection = psycopg2.connect(
+        user="postgres",
+        password="postgres",
+        host="localhost",
+        port="5432",
+        database="packages_production"
+    )
+    
+    cursor = connection.cursor() 
+
+    github_urls = []
+    with open(f"./applicationDependencies/{repo_name}/{repo_name}dependsList.txt", 'r') as depFile:
+        dependencies = depFile.read().splitlines()
+        for dep in dependencies:
+            query = f"""SELECT repository_url
+                    FROM packages
+                    WHERE name like '{dep}' AND repository_url LIKE '%github%' """
+            cursor.execute(query)
+            data = cursor.fetchall()
+            link = None
+            if data:
+                link = data[0][0]
+            if not link:
+                print(f"{dep} has no results in db")
+                github_urls.append(None)
+            else:
+                print(f"Found {link} from lookup of {dep}")
+                github_urls.append(link)
+    with open(f"./applicationDependencies/{repo_name}/{repo_name}dependMap.txt", 'w') as f:
+        for x in len(dependencies):
+            f.write(f"{dependencies[x]} {github_urls[x]}\n")
+                
+except psycopg2.Error as e:
+   print(f"Error while connecting to DB: {e}")
