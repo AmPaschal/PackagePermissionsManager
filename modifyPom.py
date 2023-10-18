ec733db86c615df038dfd19363c5080ff8362b99
Kyle <robin489@purdue.edu>
2023-10-18 01:03:09 -0400

    Removed namespace

--- modifyPom.py
+++ modifyPom.py
@@ -8,26 +8,26 @@ def modifyPom(repo_name, dir_name):
     root = tree.getroot()
     
     # Define the namespace
-    namespace = {'mvn': 'http://maven.apache.org/POM/4.0.0'}
+   # namespace = {'mvn': 'http://maven.apache.org/POM/4.0.0'}
     
     # Check if the Surefire plugin is already present
     surefire_plugin_present = False
-    for plugin in root.findall('.//mvn:plugin', namespace):
-        artifact_id = plugin.find('mvn:artifactId', namespace)
+    for plugin in root.findall('.//{http://maven.apache.org/POM/4.0.0}plugin'):
+        artifact_id = plugin.find('.//{http://maven.apache.org/POM/4.0.0}plugin')
         if artifact_id is not None and artifact_id.text == 'maven-surefire-plugin':
             surefire_plugin_present = True
             # Check if the configuration block exists
-            configuration = plugin.find('mvn:configuration', namespace)
+            configuration = plugin.find('.//{http://maven.apache.org/POM/4.0.0}plugin')
             if configuration is None:
                 configuration = ET.SubElement(plugin, 'configuration')
             # Add or update additionalClassPathElements and argLine
-            additional_classpath_elements = configuration.find('mvn:additionalClassPathElements', namespace)
+            additional_classpath_elements = configuration.find('.//{http://maven.apache.org/POM/4.0.0}plugin')
             if additional_classpath_elements is None:
                 additional_classpath_elements = ET.SubElement(configuration, 'additionalClassPathElements')
             additional_classpath_element = ET.SubElement(additional_classpath_elements, 'additionalClassPathElement')
             additional_classpath_element.text = '/home/robin489/vulnRecreation/PackagePermissionsManager/target/PackagePermissionsManager-1.0-SNAPSHOT-shaded.jar'  # Replace with your additional element path
     
-            arg_line = configuration.find('mvn:argLine', namespace)
+            arg_line = configuration.find('.//{http://maven.apache.org/POM/4.0.0}plugin')
             if arg_line is None:
                 arg_line = ET.SubElement(configuration, 'argLine')
             arg_line.text = f'-javaagent:/home/robin489/vulnRecreation/PackagePermissionsManager/target/PackagePermissionsManager-1.0-SNAPSHOT-perm-agent.jar=m10,{repo_name}'  # Replace with your desired argLine
