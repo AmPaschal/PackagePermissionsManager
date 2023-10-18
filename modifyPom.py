ab8eba7377f1b734fed1869043129744d1240ceb
Kyle <robin489@purdue.edu>
2023-10-18 02:40:02 -0400

    Davis part 2

--- modifyPom.py
+++ modifyPom.py
@@ -1,45 +1,45 @@
-import xml.etree.ElementTree as ET
+#!/usr/bin/env python3
 
-def modify_pom_xml(oldFile, newFile, repoName):
+import xml.etree.ElementTree as ET
+import sys
+
+def serialize_element(element):
+    if element.tag.startswith("{"):
+        element.tag = element.tag.split("}", 1)[1]
+    for child in element:
+        serialize_element(child)
+
+def modify_pom_xml(oldFile, newFile, repo_name):
     try:
-        # Parse the XML file
         tree = ET.parse(oldFile)
         root = tree.getroot()
+        namespaces = {'ns': 'http://maven.apache.org/POM/4.0.0'}
 
-        # Locate the <plugins> element
-        plugins = root.find('.//plugins')
+        plugins = root.find('.//ns:plugins', namespaces)
 
         if plugins is None:
-            raise Exception("No root-level <plugins> tag found.")
+            raise Exception("No <plugins> tag found.")
 
         surefire_plugin = None
-
-        # Search for maven-surefire-plugin
-        for plugin in plugins.findall('plugin'):
-            artifact_id = plugin.find('artifactId')
+        for plugin in plugins.findall('ns:plugin', namespaces):
+            artifact_id = plugin.find('ns:artifactId', namespaces)
             if artifact_id is not None and artifact_id.text == 'maven-surefire-plugin':
                 surefire_plugin = plugin
                 break
 
         if surefire_plugin is None:
-            # If plugin not found, add it
             surefire_plugin = ET.fromstring(f'''<plugin>
                 <artifactId>maven-surefire-plugin</artifactId>
                 <version>3.1.2</version>
                 <configuration>
-                    <additionalClassPathElement>/home/robin489/vulnRecreation/PackagePermissionsManager/target/PackagePermissisionsManager-1.0-shaded.jar</additionalClassPathElement>
-                    <argLine>-javaagent:/home/robin489/vulnRecreation/PackagePermissionsManager/target/PackagePermissionsManager-1.0-SNAPSHOT-perm-agent.jar=m10,{repoName}</argLine>
-                </configuration>
-                <dependencies>
+                    <additionalClassPathElement>/home/robin489/vulnRecreation/PackagePermissionsManager/target/PackagePermissionsManager-1.0-SNAPSHOT-shaded.jar</additionalClassPathElement>
+                    <argLne>-javaagent:/home/robin489/vulnRecreation/PackagePermissionsManager/target/PackagePermissionsManager-1.0-SNAPSHOT-perm-agent.jar=m10,{repo_name}</argLine>                </configuration>
             </plugin>''')
             plugins.append(surefire_plugin)
         else:
-            # If plugin found, update or append configuration
-            configuration = surefire_plugin.find('configuration')
-            
-            if configuration is not None and configuration.find('argLine') is not None:
+            configuration = surefire_plugin.find('ns:configuration', namespaces)
+            if configuration is not None and configuration.find('ns:argLine', namespaces) is not None:
                 raise Exception("argLine node already present.")
-            
             if configuration is None:
                 configuration = ET.Element('configuration')
                 surefire_plugin.append(configuration)
@@ -48,17 +48,23 @@ def modify_pom_xml(oldFile, newFile, repoName):
             if additionalClassPathElement is None:
                 additionalClassPathElement = ET.Element('additionalClassPathElement')
                 configuration.append(additionalClassPathElement)
-            additionalClassPathElement.text = '/home/robin489/vulnRecreation/PackagePermissionsManager/target/PackagePermissisionsManager-1.0-shaded.jar'
+            additionalClassPathElement.text = '/home/robin489/vulnRecreation/PackagePermissionsManager/target/PackagePermissionsManager-1.0-SNAPSHOT-shaded.jar'
+            additionalClassPathElement.tail = '\n'
             
             argLine = ET.Element('argLine')
-            argLine.text = f'-javaagent:/home/robin489/vulnRecreation/PackagePermissionsManager/target/PackagePermissionsManager-1.0-SNAPSHOT-perm-agent.jar=m10,{repoName}'
+            argLine.text = f'-javaagent:/home/robin489/vulnRecreation/PackagePermissionsManager/target/PackagePermissionsManager-1.0-SNAPSHOT-perm-agent.jar=m10,{repo_name}'
+            argLine.tail = '\n'
             configuration.append(argLine)
 
-        # Save the changes to newFile
+        # Serialize without namespaces
+        serialize_element(root)
         tree.write(newFile)
 
     except Exception as e:
         print(f"An error occurred: {e}")
 
-# Example usage
-modify_pom_xml('./junit4/pom.xml', './junit4/testpom.xml')
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    
+    modify_pom_xml("./junit4/pom.xml", "./junit4/testpom.xml","TEST")
+
