55be2005257be9daabd548cb99033afb3a100353
Kyle <robin489@purdue.edu>
2023-10-18 01:18:07 -0400

    Adding logging

--- modifyPom.py
+++ modifyPom.py
@@ -9,32 +9,39 @@ def modifyPom(repo_name, dir_name):
     
     # Define the namespace
    # namespace = {'mvn': 'http://maven.apache.org/POM/4.0.0'}
-    
+    print(f'looking for pom at {dir_name}/pom.xml')
     # Check if the Surefire plugin is already present
     surefire_plugin_present = False
     for plugin in root.findall('.//{http://maven.apache.org/POM/4.0.0}plugin'):
         artifact_id = plugin.find('.//{http://maven.apache.org/POM/4.0.0}plugin')
         if artifact_id is not None and artifact_id.text == 'maven-surefire-plugin':
             surefire_plugin_present = True
+            print("Surefire_plugin was found")
             # Check if the configuration block exists
             configuration = plugin.find('.//{http://maven.apache.org/POM/4.0.0}plugin')
             if configuration is None:
+                print("Configuration was not found creating now")
                 configuration = ET.SubElement(plugin, 'configuration')
             # Add or update additionalClassPathElements and argLine
+            
             additional_classpath_elements = configuration.find('.//{http://maven.apache.org/POM/4.0.0}plugin')
             if additional_classpath_elements is None:
+                print("Adding classpath")
                 additional_classpath_elements = ET.SubElement(configuration, 'additionalClassPathElements')
             additional_classpath_element = ET.SubElement(additional_classpath_elements, 'additionalClassPathElement')
             additional_classpath_element.text = '/home/robin489/vulnRecreation/PackagePermissionsManager/target/PackagePermissionsManager-1.0-SNAPSHOT-shaded.jar'  # Replace with your additional element path
     
             arg_line = configuration.find('.//{http://maven.apache.org/POM/4.0.0}plugin')
             if arg_line is None:
+                print("Adding arg_line")
                 arg_line = ET.SubElement(configuration, 'argLine')
             arg_line.text = f'-javaagent:/home/robin489/vulnRecreation/PackagePermissionsManager/target/PackagePermissionsManager-1.0-SNAPSHOT-perm-agent.jar=m10,{repo_name}'  # Replace with your desired argLine
             break
     
     # Write the changes back to the pom.xml file
+    print("Writing file")
     tree.write(f'{dir_name}/pom.xml', encoding='UTF-8', xml_declaration=True)
+    print("Stripping wierd extra symbols")
     with open(f'{dir_name}/pom.xml', 'r') as file:
         file_content = file.read()
 
